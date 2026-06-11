45eb3640f7874dc1585b51a2defc0d09bc889d19
Dennis Kimaita <dkimaita22@gmail.com>
2026-06-11 05:36:18 +0300

    model cache
    fix output format

--- audio_to_text.py
+++ audio_to_text.py
@@ -3,17 +3,29 @@ import whisper
 from whisper.utils import get_writer
 import os
 
+MODEL_CACHE = {}
 
-def transcribe_audio(audio_path, output_dir=None, model_size="small", translate=False):
+def get_model(model_size):
+    if model_size not in MODEL_CACHE:
+        print(f"Loading Whisper model: {model_size}")
+        MODEL_CACHE[model_size] = whisper.load_model(model_size)
+    return MODEL_CACHE[model_size]
+
+def transcribe_audio(audio_path, output_dir=None, model_size="base", translate=False):
     """
     Converts audio file to text transcript
     Returns path to transcript file or None if failed
     """
     try:
-        os.makedirs(os.path.dirname(output_dir), exist_ok=True)
+        if output_dir is None:
+            output_dir = os.path.join(os.getcwd(), "data", "audio")
 
-        print(f"Loading Whisper model ({model_size})...")
-        model = whisper.load_model(model_size)
+        os.makedirs(output_dir, exist_ok=True)
+
+        
+
+        # print(f"Loading Whisper model ({model_size})...")
+        model = get_model(model_size)
 
         print("Transcribing audio...")
         result = model.transcribe(
@@ -28,19 +40,25 @@ def transcribe_audio(audio_path, output_dir=None, model_size="small", translate=
         base_name = os.path.splitext(os.path.basename(audio_path))[0]
         txt_path = os.path.join(output_dir, f"{base_name}.txt")
         srt_path = os.path.join(output_dir, f"{base_name}.srt")
+        vtt_path = os.path.join(output_dir, f"{base_name}.vtt")
 
         # Save outputs
         with open(txt_path, "w", encoding="utf-8") as f:
             f.write(result["text"])
 
-        writer = get_writer("srt", output_dir)
-        writer(result, audio_path)
+        srt_writer = get_writer("srt", output_dir)
+        srt_writer(result, audio_path)
 
+
+        vtt_writer = get_writer("vtt", output_dir)
+        vtt_writer(result, audio_path)
+        
         print(f"Transcript saved to: {txt_path}")
-        print(f"Subtitles saved to: {srt_path}")
+        print(f"SRT Subtitles saved to: {srt_path}")
+        print(f"VTT Subtitles saved to: {vtt_path}")
 
-        return txt_path, language
+        return txt_path, srt_path, vtt_path, language
 
     except Exception as e:
         print(f"Transcription error: {str(e)}")
-        return None, None
+        return None, None, None, None
